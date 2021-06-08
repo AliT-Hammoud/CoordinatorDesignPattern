c276779a31b298aaaa1b1b0f655a2c04c2018ae6
AliT-Hammoud <ali.ha@totersapp.com>
2021-06-08 19:24:42 +0300

    Initial Commit

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Ali Hammoud on 6/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
